7757e74e2a99fb2063b4dc8830c027400ba391fc
furkandeveloper <Furkan.dvlp@gmail.com>
2019-07-04 16:32:13 +0300

    customerAdd screen create

--- forms_demo/lib/screens/customerAdd.dart
+++ forms_demo/lib/screens/customerAdd.dart
@@ -1,27 +1,98 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class CustomerAdd extends StatefulWidget{
+class CustomerAdd extends StatefulWidget {
   @override
-  State<StatefulWidget> createState()=>CustomerAddState();
-} 
+  State<StatefulWidget> createState() => CustomerAddState();
+}
 
-class CustomerAddState extends State{
+class CustomerAddState extends State {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(13.0),
+      padding: const EdgeInsets.all(8.0),
       child: Container(
         child: Center(
           child: Form(
             child: Column(
-            children: <Widget>[
-              Text("Customer Create",textDirection: TextDirection.ltr,)
-            ],
-          ),
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: firstNameField(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: lastNameField(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: emailNameField(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: passwordNameField(),
+                ),
+                submitButton()
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
+  Widget firstNameField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: "First Name",
+        hintText: "Jhon",
+        contentPadding:
+            new EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(16.0)),
+      ),
+    );
+  }
+
+  Widget lastNameField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: "Last Name",
+        hintText: "Doe",
+        contentPadding:
+            new EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(16.0)),
+      ),
+    );
+  }
+
+  Widget emailNameField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: "Email",
+        hintText: "mail@gmail.com",
+        contentPadding:
+            new EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(16.0)),
+      ),
+    );
+  }
+
+  Widget passwordNameField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: "Password",
+        hintText: "****",
+        contentPadding:
+            new EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(16.0)),
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return FlatButton(
+      child: Text("Ekle"),
+      onPressed: () {},
+    );
+  }
 }
